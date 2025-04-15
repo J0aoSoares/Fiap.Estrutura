861d256ce8885ac8e2795a2375c29d1ad48d6c17
labsfiap <labsfiap@PA105MICRO13>
2025-04-15 20:46:09 -0300

    Add project files.

---
+++ Fiap.Estrutura.UI/Program.cs
@@ -0,0 +1,7 @@
+﻿using Fiap.Estrutura.Model;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Funcionario func = new Funcionario();
+func.cpf = "56969612821";
